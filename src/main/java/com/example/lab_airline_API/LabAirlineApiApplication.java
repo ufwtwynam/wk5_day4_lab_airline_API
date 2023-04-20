0f190f46cade39478eaa6b2a5592828576d39995
Umar Twynam <umartwynam@gmail.com>
2023-04-20 16:42:18 +0100

    initial upload

---
+++ src/main/java/com/example/lab_airline_API/LabAirlineApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab_airline_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabAirlineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabAirlineApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab_airline_API/LabAirlineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab_airline_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabAirlineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
