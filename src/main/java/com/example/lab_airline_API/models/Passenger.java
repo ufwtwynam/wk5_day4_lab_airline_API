1597cdd7d14d9e75e511ca6f8ae5b00d71efb4e8
Umar Twynam <umartwynam@gmail.com>
2023-04-20 18:03:01 +0100

    passenger table columns

--- src/main/java/com/example/lab_airline_API/models/Passenger.java
+++ src/main/java/com/example/lab_airline_API/models/Passenger.java
@@ -1,9 +1,19 @@
 package com.example.lab_airline_API.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "passengers")
 public class Passenger {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column(name = "email_address")
+    private String emailAddress;
 }
